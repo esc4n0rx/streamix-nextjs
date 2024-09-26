a7cacae286f1e358b5448e865ea0733e38d23b1f
esc4n0rx <paulo.cunha@hortifruti.com.br>
2024-09-26 16:01:13 -0300

    Fix middleware

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,18 +1,17 @@
-import { NextResponse } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
 
-export async function middleware(req: NextRequest) { 
-    const res = NextResponse.next();
-  
-    const supabaseToken = req.cookies.get('sb-access-token')?.value;
-  
-    if (!supabaseToken) {
-      return NextResponse.redirect(new URL('/login', req.url));
-    }
-  
-    return res;
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+
+  const supabaseToken = req.cookies.get('sb-access-token')?.value;
+
+  if (!supabaseToken) {
+    return NextResponse.redirect(new URL('/login', req.url));
   }
 
+  return res;
+}
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/player/:path*'], 
+  matcher: ['/dashboard/:path*', '/player/:path*'],
 };
